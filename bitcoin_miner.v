595624dbb970415cdef4aac121563c8fa04af1a8
Christopher Streiffer <cds33@duke.edu>
2015-04-15 01:14:04 -0400

    Fixed errors

--- bitcoin_miner.v
+++ bitcoin_miner.v
@@ -1,8 +1,8 @@
-module bitcoin_miner(in0, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, clk, out);
+module bitcoin_miner(in0, in1, in2, in3, clk, out);
 	input clk;
-	input [31:0] in0, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10;
-	wire [31:0] in11, in12, in13, in14, in15;
-	output [31:0] out;
+	input [31:0] in0, in1, in2, in3;
+	wire [31:0] in4, in5, in6, in7, in8, in9, in10, in11, in12, in13, in14, in15;
+	output [255:0] out;
 	
 	wire [255:0] rh_out, rs;
 	wire [511:0] rd_out;
@@ -13,55 +13,54 @@ module bitcoin_miner(in0, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, clk
 	
 	reg_h rh(clr, clk, clk_en, rh_out); 
 	reg_k rk(clr, clk, clk_en, rk_out);
-	
 	reg_w rw(rd_out, clr, clk, clk_en, rw_out);
 	
-	wire c0_in, c0_out;
+	wire [255:0] c0_in, c0_out;
 	reg_ints    r0(rh_out, clr, clk, clk_en, c0_in);
 	compression c0(c0_in, rw_out[31:0], rk_out[31:0], c0_out);
 	
-	wire c1_in, c1_out;
+	/*
+	wire [255:0] c1_in, c1_out;
 	reg_ints    r1(c0_out, clr, clk, clk_en, c1_in);
 	compression c1(c1_in, rw_out[63:32], rk_out[63:32], c1_out);
 	
-	
-	wire c2_in, c2_out;
+	wire [255:0] c2_in, c2_out;
 	reg_ints    r2(c1_out, clr, clk, clk_en, c2_in);
 	compression c2(c2_in, rw_out[95:64], rk_out[95:64], c2_out);
 	
-	wire c3_in, c3_out;
+	wire [255:0] c3_in, c3_out;
 	reg_ints    r3(c2_out, clr, clk, clk_en, c3_in);
 	compression c3(c3_in, rw_out[127:96], rk_out[127:96], c3_out);
 	
-	wire c4_in, c4_out;
+	wire [255:0] c4_in, c4_out;
 	reg_ints    r4(c3_out, clr, clk, clk_en, c4_in);
 	compression c4(c4_in, rw_out[159:128], rk_out[159:128], c4_out);
 	
-	wire c5_in, c5_out;
+	wire [255:0] c5_in, c5_out;
 	reg_ints    r5(c4_out, clr, clk, clk_en, c5_in);
 	compression c5(c5_in, rw_out[191:160], rk_out[191:160], c5_out);
 	
-	wire c6_in, c6_out;
+	wire [255:0] c6_in, c6_out;
 	reg_ints    r6(c5_out, clr, clk, clk_en, c6_in);
 	compression c6(c6_in, rw_out[223:192], rk_out[223:192], c6_out);
 	
-	wire c7_in, c7_out;
+	wire [255:0] c7_in, c7_out;
 	reg_ints    r7(c6_out, clr, clk, clk_en, c7_in);
 	compression c7(c7_in, rw_out[255:224], rk_out[255:224], c7_out);
 	
-	wire c8_in, c8_out;
+	wire [255:0] c8_in, c8_out;
 	reg_ints    r8(c7_out, clr, clk, clk_en, c8_in);
 	compression c8(c8_in, rw_out[287:256], rk_out[287:256], c8_out);
 	
-	wire c9_in, c9_out;
+	wire [255:0] c9_in, c9_out;
 	reg_ints    r9(c8_out, clr, clk, clk_en, c9_in);
 	compression c9(c9_in, rw_out[319:288], rk_out[319:288], c9_out);
 	
-	wire c10_in, c10_out;
+	wire [255:0] c10_in, c10_out;
 	reg_ints    r10(c9_out, clr, clk, clk_en, c10_in);
 	compression c10(c10_in, rw_out[351:320], rk_out[351:320], c10_out);
 	
-	wire c11_in, c11_out;
+	wire [255:0] c11_in, c11_out;
 	reg_ints    r11(c10_out, clr, clk, clk_en, c11_in);
 	compression c11(c11_in, rw_out[383:352], rk_out[383:352], c11_out);
 	
@@ -69,210 +68,219 @@ module bitcoin_miner(in0, in1, in2, in3, in4, in5, in6, in7, in8, in9, in10, clk
 	reg_ints    r12(c11_out, clr, clk, clk_en, c12_in);
 	compression c12(c12_in, rw_out[415:384], rk_out[415:384], c12_out);
 	
-	wire c13_in, c13_out;
+	wire [255:0] c13_in, c13_out;
 	reg_ints    r13(c12_out, clr, clk, clk_en, c13_in);
 	compression c13(c13_in, rw_out[447:416], rk_out[447:416], c13_out);
 	
-	wire c14_in, c14_out;
+	wire [255:0] c14_in, c14_out;
 	reg_ints    r14(c13_out, clr, clk, clk_en, c14_in);
 	compression c14(c14_in, rw_out[479:448], rk_out[479:448], c14_out);
 	
-	wire c15_in, c15_out;
+	wire [255:0] c15_in, c15_out;
 	reg_ints    r15(c14_out, clr, clk, clk_en, c15_in);
 	compression c15(c15_in, rw_out[511:480], rk_out[511:480], c15_out);
 	
-	wire c16_in, c16_out;
+	wire [255:0] c16_in, c16_out;
 	reg_ints    r16(c15_out, clr, clk, clk_en, c16_in);
 	compression c16(c16_in, rw_out[543:512], rk_out[543:512], c16_out);
 	
-	wire c17_in, c17_out;
+	wire [255:0] c17_in, c17_out;
 	reg_ints    r17(c16_out, clr, clk, clk_en, c17_in);
 	compression c17(c17_in, rw_out[575:544], rk_out[575:544], c17_out);
 	
-	wire c18_in, c18_out;
+	wire [255:0] c18_in, c18_out;
 	reg_ints    r18(c17_out, clr, clk, clk_en, c18_in);
 	compression c18(c18_in, rw_out[607:576], rk_out[607:576], c18_out);
 	
-	wire c19_in, c19_out;
+	wire [255:0] c19_in, c19_out;
 	reg_ints    r19(c18_out, clr, clk, clk_en, c19_in);
 	compression c19(c19_in, rw_out[639:608], rk_out[639:608], c19_out);
 	
-	wire c20_in, c20_out;
+	wire [255:0] c20_in, c20_out;
 	reg_ints    r20(c19_out, clr, clk, clk_en, c20_in);
 	compression c20(c20_in, rw_out[671:640], rk_out[671:640], c20_out);
 	
-	wire c21_in, c21_out;
+	wire [255:0] c21_in, c21_out;
 	reg_ints    r21(c20_out, clr, clk, clk_en, c21_in);
 	compression c21(c21_in, rw_out[703:672], rk_out[703:672], c21_out);
 	
-	wire c22_in, c22_out;
+	wire [255:0] c22_in, c22_out;
 	reg_ints    r22(c21_out, clr, clk, clk_en, c22_in);
 	compression c22(c22_in, rw_out[735:704], rk_out[735:704], c22_out);
 	
-	wire c23_in, c23_out;
+	wire [255:0] c23_in, c23_out;
 	reg_ints    r23(c22_out, clr, clk, clk_en, c23_in);
 	compression c23(c23_in, rw_out[767:736], rk_out[767:736], c23_out);
 	
-	wire c24_in, c24_out;
+	wire [255:0] c24_in, c24_out;
 	reg_ints    r24(c23_out, clr, clk, clk_en, c24_in);
 	compression c24(c24_in, rw_out[799:768], rk_out[799:768], c24_out);
 	
-	wire c25_in, c25_out;
+	wire [255:0] c25_in, c25_out;
 	reg_ints    r25(c24_out, clr, clk, clk_en, c25_in);
 	compression c25(c25_in, rw_out[831:800], rk_out[831:800], c25_out);
 	
-	wire c26_in, c26_out;
+	wire [255:0] c26_in, c26_out;
 	reg_ints    r26(c25_out, clr, clk, clk_en, c26_in);
 	compression c26(c26_in, rw_out[863:832], rk_out[863:832], c26_out);
 	
-	wire c27_in, c27_out;
+	wire [255:0] c27_in, c27_out;
 	reg_ints    r27(c26_out, clr, clk, clk_en, c27_in);
 	compression c27(c27_in, rw_out[895:864], rk_out[895:864], c27_out);
 	
-	wire c28_in, c28_out;
+	wire [255:0] c28_in, c28_out;
 	reg_ints    r28(c27_out, clr, clk, clk_en, c28_in);
 	compression c28(c28_in, rw_out[927:896], rk_out[927:896], c28_out);
 	
-	wire c29_in, c29_out;
+	wire [255:0] c29_in, c29_out;
 	reg_ints    r29(c28_out, clr, clk, clk_en, c29_in);
 	compression c29(c29_in, rw_out[959:928], rk_out[959:928], c29_out);
 	
-	wire c30_in, c30_out;
+	wire [255:0] c30_in, c30_out;
 	reg_ints    r30(c29_out, clr, clk, clk_en, c30_in);
 	compression c30(c30_in, rw_out[991:960], rk_out[991:960], c30_out);
 	
-	wire c31_in, c31_out;
+	wire [255:0] c31_in, c31_out;
 	reg_ints    r31(c30_out, clr, clk, clk_en, c31_in);
 	compression c31(c31_in, rw_out[1023:992], rk_out[1023:992], c31_out);
 	
-	wire c32_in, c32_out;
+	wire [255:0] c32_in, c32_out;
 	reg_ints    r32(c31_out, clr, clk, clk_en, c32_in);
 	compression c32(c32_in, rw_out[1055:1024], rk_out[1055:1024], c32_out);
 	
-	wire c33_in, c33_out;
+	wire [255:0] c33_in, c33_out;
 	reg_ints    r33(c32_out, clr, clk, clk_en, c33_in);
 	compression c33(c33_in, rw_out[1087:1056], rk_out[1087:1056], c33_out);
 	
-	wire c34_in, c34_out;
+	wire [255:0] c34_in, c34_out;
 	reg_ints    r34(c33_out, clr, clk, clk_en, c34_in);
 	compression c34(c34_in, rw_out[1119:1088], rk_out[1119:1088], c34_out);
 	
-	wire c35_in, c35_out;
+	wire [255:0] c35_in, c35_out;
 	reg_ints    r35(c34_out, clr, clk, clk_en, c35_in);
 	compression c35(c35_in, rw_out[1151:1120], rk_out[1151:1120], c35_out);
 	
-	wire c36_in, c36_out;
+	wire [255:0] c36_in, c36_out;
 	reg_ints    r36(c35_out, clr, clk, clk_en, c36_in);
 	compression c36(c36_in, rw_out[1183:1152], rk_out[1183:1152], c36_out);
 	
-	wire c37_in, c37_out;
+	wire [255:0] c37_in, c37_out;
 	reg_ints    r37(c36_out, clr, clk, clk_en, c37_in);
 	compression c37(c37_in, rw_out[1215:1184], rk_out[1215:1184], c37_out);
 	
-	wire c38_in, c38_out;
+	wire [255:0] c38_in, c38_out;
 	reg_ints    r38(c37_out, clr, clk, clk_en, c38_in);
 	compression c38(c38_in, rw_out[1247:1216], rk_out[1247:1216], c38_out);
 	
-	wire c39_in, c39_out;
+	wire [255:0] c39_in, c39_out;
 	reg_ints    r39(c38_out, clr, clk, clk_en, c39_in);
 	compression c39(c39_in, rw_out[1279:1248], rk_out[1279:1248], c39_out);
 	
-	wire c40_in, c40_out;
+	wire [255:0] c40_in, c40_out;
 	reg_ints    r40(c39_out, clr, clk, clk_en, c40_in);
 	compression c40(c40_in, rw_out[1311:1280], rk_out[1311:1280], c40_out);
 	
-	wire c41_in, c41_out;
+	wire [255:0] c41_in, c41_out;
 	reg_ints    r41(c40_out, clr, clk, clk_en, c41_in);
 	compression c41(c41_in, rw_out[1343:1312], rk_out[1343:1312], c41_out);
 	
-	wire c42_in, c42_out;
+	wire [255:0] c42_in, c42_out;
 	reg_ints    r42(c41_out, clr, clk, clk_en, c42_in);
 	compression c42(c42_in, rw_out[1375:1344], rk_out[1375:1344], c42_out);
 	
-	wire c43_in, c43_out;
+	wire [255:0] c43_in, c43_out;
 	reg_ints    r43(c42_out, clr, clk, clk_en, c43_in);
 	compression c43(c43_in, rw_out[1407:1376], rk_out[1407:1376], c43_out);
 	
-	wire c44_in, c44_out;
+	wire [255:0] c44_in, c44_out;
 	reg_ints    r44(c43_out, clr, clk, clk_en, c44_in);
 	compression c44(c44_in, rw_out[1439:1408], rk_out[1439:1408], c44_out);
 	
-	wire c45_in, c45_out;
+	wire [255:0] c45_in, c45_out;
 	reg_ints    r45(c44_out, clr, clk, clk_en, c45_in);
 	compression c45(c45_in, rw_out[1471:1440], rk_out[1471:1440], c45_out);
 	
-	wire c46_in, c46_out;
+	wire [255:0] c46_in, c46_out;
 	reg_ints    r46(c45_out, clr, clk, clk_en, c46_in);
 	compression c46(c46_in, rw_out[1503:1472], rk_out[1503:1472], c46_out);
 	
-	wire c47_in, c47_out;
+	wire [255:0] c47_in, c47_out;
 	reg_ints    r47(c46_out, clr, clk, clk_en, c47_in);
 	compression c47(c47_in, rw_out[1535:1504], rk_out[1535:1504], c47_out);
 	
-	wire c48_in, c48_out;
+	wire [255:0] c48_in, c48_out;
 	reg_ints    r48(c47_out, clr, clk, clk_en, c48_in);
 	compression c48(c48_in, rw_out[1567:1536], rk_out[1567:1536], c48_out);
 	
-	wire c49_in, c49_out;
+	wire [255:0] c49_in, c49_out;
 	reg_ints    r49(c48_out, clr, clk, clk_en, c49_in);
 	compression c49(c49_in, rw_out[1599:1568], rk_out[1599:1568], c49_out);
 	
-	wire c50_in, c50_out;
+	wire [255:0] c50_in, c50_out;
 	reg_ints    r50(c49_out, clr, clk, clk_en, c50_in);
 	compression c50(c50_in, rw_out[1631:1600], rk_out[1631:1600], c50_out);
 	
-	wire c51_in, c51_out;
+	wire [255:0] c51_in, c51_out;
 	reg_ints    r51(c50_out, clr, clk, clk_en, c51_in);
 	compression c51(c51_in, rw_out[1663:1632], rk_out[1663:1632], c51_out);
 	
-	wire c52_in, c52_out;
+	wire [255:0] c52_in, c52_out;
 	reg_ints    r52(c51_out, clr, clk, clk_en, c52_in);
 	compression c52(c52_in, rw_out[1695:1664], rk_out[1695:1664], c52_out);
 	
-	wire c53_in, c53_out;
+	wire [255:0] c53_in, c53_out;
 	reg_ints    r53(c52_out, clr, clk, clk_en, c53_in);
 	compression c53(c53_in, rw_out[1727:1696], rk_out[1727:1696], c53_out);
 	
-	wire c54_in, c54_out;
+	wire [255:0] c54_in, c54_out;
 	reg_ints    r54(c53_out, clr, clk, clk_en, c54_in);
 	compression c54(c54_in, rw_out[1759:1728], rk_out[1759:1728], c54_out);
 	
-	wire c55_in, c55_out;
+	wire [255:0] c55_in, c55_out;
 	reg_ints    r55(c54_out, clr, clk, clk_en, c55_in);
 	compression c55(c55_in, rw_out[1791:1760], rk_out[1791:1760], c55_out);
 	
-	wire c56_in, c56_out;
+	wire [255:0] c56_in, c56_out;
 	reg_ints    r56(c55_out, clr, clk, clk_en, c56_in);
 	compression c56(c56_in, rw_out[1823:1792], rk_out[1823:1792], c56_out);
 	
-	wire c57_in, c57_out;
+	wire [255:0] c57_in, c57_out;
 	reg_ints    r57(c56_out, clr, clk, clk_en, c57_in);
 	compression c57(c57_in, rw_out[1855:1824], rk_out[1855:1824], c57_out);
 	
-	wire c58_in, c58_out;
+	wire [255:0] c58_in, c58_out;
 	reg_ints    r58(c57_out, clr, clk, clk_en, c58_in);
 	compression c58(c58_in, rw_out[1887:1856], rk_out[1887:1856], c58_out);
 	
-	wire c59_in, c59_out;
+	wire [255:0] c59_in, c59_out;
 	reg_ints    r59(c58_out, clr, clk, clk_en, c59_in);
 	compression c59(c59_in, rw_out[1919:1888], rk_out[1919:1888], c59_out);
 	
-	wire c60_in, c60_out;
+	wire [255:0] c60_in, c60_out;
 	reg_ints    r60(c59_out, clr, clk, clk_en, c60_in);
 	compression c60(c60_in, rw_out[1951:1920], rk_out[1951:1920], c60_out);
 	
-	wire c61_in, c61_out;
+	wire [255:0] c61_in, c61_out;
 	reg_ints    r61(c60_out, clr, clk, clk_en, c61_in);
 	compression c61(c61_in, rw_out[1983:1952], rk_out[1983:1952], c61_out);
 	
-	wire c62_in, c62_out;
+	wire [255:0] c62_in, c62_out;
 	reg_ints    r62(c61_out, clr, clk, clk_en, c62_in);
 	compression c62(c62_in, rw_out[2015:1984], rk_out[2015:1984], c62_out);
 	
-	wire c63_in, c63_out;
+	wire [255:0] c63_in, c63_out;
 	reg_ints    r63(c62_out, clr, clk, clk_en, c63_in);
 	compression c63(c63_in, rw_out[2047:2016], rk_out[2047:2016], c63_out);
 	*/
+	register x1(c63_out[31:0],    rh_out[31:0],    out[31:0]); 
+	register x2(c63_out[63:32],   rh_out[63:32],   out[63:32]);
+	register x3(c63_out[95:64],   rh_out[95:64],   out[95:64]);
+	register x4(c63_out[127:96],  rh_out[127:96],  out[127:96]);
+	register x5(c63_out[159:128], rh_out[159:128], out[159:128]);
+	register x6(c63_out[191:160], rh_out[191:160], out[191:160]);
+	register x7(c63_out[223:192], rh_out[223:192], out[223:192]);
+	register x8(c63_out[255:224], rh_out[255:224], out[255:224]);
+
 endmodule
 
 // Different blocks
@@ -511,6 +519,7 @@ endmodule
 module s1(in, out);
 	input [31:0] in;
 	output [31:0] out;
+	
 	// Create the shift values
 	wire [4:0] s17, s19, s10;
 	assign s17 = 5'b10001;
